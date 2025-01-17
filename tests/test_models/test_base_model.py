7c784c1639c6a72365de9851cc684f90221cb35a
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-01-17 20:29:41 +0200

    testing save on base_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 
 import unittest
+from unittest import mock
 from datetime import datetime
 import sys
 import os
+import models
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 from models.base_model import BaseModel
+
 # from models.base_model import BaseModel
+
 
 class TestBaseModel(unittest.TestCase):
 
@@ -16,7 +20,8 @@ class TestBaseModel(unittest.TestCase):
 
     def tearDown(self): ...
 
-    def test_save(self):
+    @mock.patch("models.storage")
+    def test_save(self, mock_storage):
         # testing if save is appropriately updating time..
         time_1 = self.sample_base.created_at
         self.sample_base.save()
@@ -27,6 +32,10 @@ class TestBaseModel(unittest.TestCase):
         self.sample_base.save()
         time_3 = self.sample_base.updated_at
         self.assertIsNot(time_2, time_3)
+
+        # assert new and save was called
+        self.assertTrue(mock_storage.new.called)
+        self.assertTrue(mock_storage.save.called)
 
     def test_to_dict(self):
         # testing to_dict
