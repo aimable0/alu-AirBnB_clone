e8fea8b14ea6e5e50d609a126338fc1d765deb36
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2025-01-17 18:42:30 +0200

    test phase 1

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -16,19 +16,17 @@ class TestUser(unittest.TestCase):
     #     self.key = self.instance.__class__.__name__ + "." + str(self.instance.id)
     #     storage.delete(self.key)
 
-    # def test_user(self):
-    #     self.assertTrue(issubclass(User, BaseModel))
-    #     self.assertIsInstance(self.instance, BaseModel)
-    #     self.assertTrue(hasattr(self.instance, "id"))
-    #     self.assertTrue(hasattr(self.instance, "created_at"))
-    #     self.assertTrue(hasattr(self.instance, "updated_at"))
+    def test_user(self):
+        self.assertTrue(issubclass(User, BaseModel))
+        self.assertIsInstance(self.instance, BaseModel)
+        self.assertTrue(hasattr(self.instance, "id"))
+        self.assertTrue(hasattr(self.instance, "created_at"))
+        self.assertTrue(hasattr(self.instance, "updated_at"))
 
     def test_email(self):
         self.assertTrue(type(self.instance), User)
         self.assertTrue(hasattr(self.instance, "email"))
         self.assertTrue(User.email == "")
-
-
         # check for email attribute after saving
         # self.instance.save()
         # all_objs = storage.all()
@@ -38,26 +36,29 @@ class TestUser(unittest.TestCase):
         # self.assertTrue(hasattr(instance_obj, "email"))
         # storage.delete(key)
 
-    # def test_password(self):
-    #     self.assertTrue(type(self.instance), User)
-    #     self.assertTrue(hasattr(self.instance, "password"))
+    def test_password(self):
+        self.assertTrue(type(self.instance), User)
+        self.assertTrue(hasattr(self.instance, "password"))
+        self.assertTrue(User.password == "")
 
-    #     # check for email attribute after saving
-    #     self.instance.save()
-    #     all_objs = storage.all()
-    #     key = "User." + str(self.instance.id)
-    #     instance_obj = all_objs[key]
-    #     self.assertTrue(key in all_objs.keys())
-    #     self.assertTrue(hasattr(instance_obj, "password"))
-    #     storage.delete(key)
+        # check for email attribute after saving
+        # self.instance.save()
+        # all_objs = storage.all()
+        # key = "User." + str(self.instance.id)
+        # instance_obj = all_objs[key]
+        # self.assertTrue(key in all_objs.keys())
+        # self.assertTrue(hasattr(instance_obj, "password"))
+        # storage.delete(key)
 
-    # def test_first_name(self):
-    #     self.assertTrue(type(self.instance), User)
-    #     self.assertTrue(hasattr(self.instance, "first_name"))
+    def test_first_name(self):
+        self.assertTrue(User.first_name == "")
+        self.assertTrue(type(self.instance), User)
+        self.assertTrue(hasattr(self.instance, "first_name"))
 
-    # def test_last_name(self):
-    #     self.assertTrue(type(self.instance), User)
-    #     self.assertTrue(hasattr(self.instance, "last_name"))
+    def test_last_name(self):
+        self.assertTrue(User.last_name == "")
+        self.assertTrue(type(self.instance), User)
+        self.assertTrue(hasattr(self.instance, "last_name"))
 
 
 if __name__ == "__main__":
